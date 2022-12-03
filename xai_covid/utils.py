28df6ee579c6bca94fe586def6263dca07b3ea30
Krystian-Krol <krystian.krol@op.pl>
2022-12-03 22:40:07 +0100

    CRP Hack classes

---
+++ xai_covid/utils.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+WIDTH = 256
+HEIGHT = 256
+ROOT = "data/train/train/"
+
+def stacked_img(img_path, extra_img):
+    img = Image.open(ROOT + img_path + ".jpg")
+
+    new_im = Image.new('RGB', (2 * WIDTH, HEIGHT))
+    new_im.paste(img, (0, 0))
+    new_im.paste(extra_img, (WIDTH, 0))
+    return new_im
+    
+def iterate_class(dataset, find_melanoma=1):
+    for idx in range(len(dataset)):
+        if dataset[idx][1] == find_melanoma:
+            yield dataset[idx][0].view(1, 3, 256, 256), dataset.lookup_path(idx)
