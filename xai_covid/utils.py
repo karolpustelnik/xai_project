a5e48690c449965880ac7b82655907b0850e3d1b
Krystian-Krol <krystian.kytek@gmail.com>
2023-02-03 23:39:03 +0100

    Last commit

--- xai_covid/utils.py
+++ xai_covid/utils.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 WIDTH = 256
 HEIGHT = 256
-ROOT = "/Users/karol/melanoma/train/train/"
+ROOT = "data/train/train/"
 
 def stacked_img(img_path, extra_img):
     img = Image.open(ROOT + img_path + ".jpg")
